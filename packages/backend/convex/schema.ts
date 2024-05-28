8f21a0ff2e89ea619ce05099c04a03c3c77bbe90
Anthony Burns <anthonylouisburns@gmail.com>
2024-05-28 11:14:42 -0400

    MVP 0.1 web

--- packages/backend/convex/schema.ts
+++ packages/backend/convex/schema.ts
@@ -8,4 +8,30 @@ export default defineSchema({
     content: v.string(),
     summary: v.optional(v.string()),
   }),
+  podcast: defineTable({
+    name: v.string(),
+    rss_url: v.string(),
+    rss_body: v.optional(v.id("_storage")),
+    number_of_episodes: v.optional(v.number()),
+    timeSpans: v.optional(v.array(v.object({
+      name: v.string(),
+      start: v.string(),
+      end: v.string(),
+    }))),
+  })
+  .index("rss_url", ["rss_url"]),
+  episode: defineTable({
+    podcast_id: v.id("podcast"),
+    episode_number: v.number(),
+    // title: v.string(),
+    // pud_date: v.string(),
+    // media_url: v.string(),
+    body: v.any(),
+    timeSpans: v.optional(v.array(v.object({
+      name: v.string(),
+      start: v.string(),
+      end: v.string(),
+    }))),
+  })
+  .index("podcast_episode_number", ["podcast_id", "episode_number"]),
 });
