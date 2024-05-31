463d3ce5dfb579bcd549e1ef47071c8bca1de68b
Anthony Louis Burns, Esq <burnstony@gmail.com>
2024-05-31 15:08:46 -0400

    clerk login working again update dependencies

--- apps/native/src/screens/Timeline.tsx
+++ apps/native/src/screens/Timeline.tsx
@@ -1,27 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
-  StyleSheet,
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  Image,
-  FlatList,
-  Dimensions,
+  Text
 } from 'react-native';
-import { Feather, AntDesign } from '@expo/vector-icons';
-import { RFValue } from 'react-native-responsive-fontsize';
-import { useAuth, useUser } from '@clerk/clerk-expo';
-import { api } from '@packages/backend/convex/_generated/api';
-import { useQuery } from 'convex/react';
 
-const Timeline = ({ navigation }) => {
-  const user = useUser();
-  const imageUrl = user?.user?.imageUrl;
-  const firstName = user?.user?.firstName;
-
-  const timeline = useQuery(api.everwzh.timeline);
-  const [search, setSearch] = useState('');
+const Timeline = () => {
 
  
 
