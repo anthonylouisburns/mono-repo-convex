cc91d437fbdb2abb2ddf8fab885c7716cb950511
Anthony Louis Burns, Esq <burnstony@gmail.com>
2024-06-07 11:27:23 -0400

    app minimum nav with min info

--- apps/native/src/screens/Episodes.tsx
+++ apps/native/src/screens/Episodes.tsx
@@ -12,12 +12,15 @@ import { useAuth, useUser } from '@clerk/clerk-expo';
 import { api } from '@packages/backend/convex/_generated/api';
 import { useQuery } from 'convex/react';
 import { EverwhzHeader, styles } from '../component/EverwhzHeader';
+import { Id } from '@packages/backend/convex/_generated/dataModel';
+import HTMLView from 'react-native-htmlview';
 
-const Timeline = ({ navigation }) => {
+const Episodes = ({ route, navigation }) => {
   const user = useUser();
   const imageUrl = user?.user?.imageUrl;
   const firstName = user?.user?.firstName;
-  const { isLoaded,signOut } = useAuth();
+  const { isLoaded, } = useAuth();
+  const { page, podcast_id, podcast_name } = route.params;
 
   if (!isLoaded) {
     return null;
@@ -25,15 +28,22 @@ const Timeline = ({ navigation }) => {
 
 
 
+  const episodes = useQuery(api.everwzh.episodes, { podcast_id: podcast_id as Id<"podcast"> })
+  const itemView = episodes ? episodes.map((episode) => (
+    <View style={styles.container}>
+      <HTMLView value={episode?.body.title} />
+    </View>
+  )) : []
+
+
+
 
   return (
     <View style={styles.container}>
 
-      <EverwhzHeader navigation={navigation} page={"episodes"}/>
-    
-          <Text>hi tony episodes</Text>
-
-
+      <EverwhzHeader navigation={navigation} page="episodes" />
+      <Text>{podcast_name}</Text>
+      {itemView}
 
     </View>
   );
@@ -41,4 +51,4 @@ const Timeline = ({ navigation }) => {
 
 
 
-export default Timeline;
+export default Episodes;
--- apps/native/src/screens/Timeline.tsx
+++ apps/native/src/screens/Timeline.tsx
@@ -12,6 +12,8 @@ import { useAuth, useUser } from '@clerk/clerk-expo';
 import { api } from '@packages/backend/convex/_generated/api';
 import { useQuery } from 'convex/react';
 import { EverwhzHeader, styles } from '../component/EverwhzHeader';
+import HTMLView from 'react-native-htmlview';
+import { timedisplay } from "@packages/backend/utilities/utility";
 
 const Timeline = ({ navigation }) => {
   const user = useUser();
@@ -23,17 +25,25 @@ const Timeline = ({ navigation }) => {
     return null;
   }
 
+  const spans = useQuery(api.everwzh.timeline)
 
+  const spansView = spans ? spans.map((span) => (
+    <View style={styles.container}>
+    <Text style={styles.link}
+      onPress={() =>
+        navigation.navigate('Episodes', {page: "episodes", podcast_id:span.podcast._id, podcast_name:span.podcast.name})
+      }
+    >{span.podcast.name}</Text> 
+    <HTMLView value={span.episode?.body.title}/>
+    <Text>{timedisplay(span.span.start)} to {timedisplay(span.span.end)} {span.span.name}</Text>
+    </View>
+  )): []
 
 
   return (
     <View style={styles.container}>
-
       <EverwhzHeader navigation={navigation} page={"timeline"}/>
-      <Text>hi tony timeline</Text>
-
-
-
+      {spansView}
     </View>
   );
 };
