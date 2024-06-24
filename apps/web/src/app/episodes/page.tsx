7d07cb0634df41af8dc237bd93d5df7084a08438
Anthony Louis Burns, Esq <burnstony@gmail.com>
2024-06-24 16:27:12 -0400

    deploy

--- apps/web/src/app/episodes/page.tsx
+++ apps/web/src/app/episodes/page.tsx
@@ -4,7 +4,6 @@ import { useQuery, } from 'convex/react';
 import { api } from '@packages/backend/convex/_generated/api';
 import { useSearchParams } from 'next/navigation'
 import { Id } from '@packages/backend/convex/_generated/dataModel';
-import EverwhzHeader from '@/components/EverwhzHeader';
 import { Episode } from '../timeline/Episode';
 
 export default function Page() {
