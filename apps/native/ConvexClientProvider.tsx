f3ae9983b92f90ea0a722e5dc34803828d13d841
Anthony Louis Burns, Esq <burnstony@gmail.com>
2024-06-26 17:39:41 -0400

    build native for deploy

--- apps/native/ConvexClientProvider.tsx
+++ apps/native/ConvexClientProvider.tsx
@@ -3,6 +3,7 @@
 import { ConvexReactClient } from 'convex/react';
 import { ConvexProviderWithClerk } from 'convex/react-clerk';
 import { ClerkProvider, useAuth } from '@clerk/clerk-expo';
+import React from 'react';
 
 const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL);
 
