7329a392540c3743750bf858bae9fc73b26f403d
Anthony Louis Burns, Esq <burnstony@gmail.com>
2024-07-01 20:05:21 -0400

    buildign for release to apple

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -8,6 +8,7 @@ import Base from './Base';
 import Head from 'next/head';
 import { useState } from 'react';
 import PlayerHolder from '@/components/PlayerHolder';
+import Link from 'next/link';
 
 
 export const metadata: Metadata = {
@@ -25,18 +26,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
 
-  console.log("process.env.CONVEX_URL",process.env.CONVEX_URL!)
+  console.log("process.env.CONVEX_URL", process.env.CONVEX_URL!)
   return (
     <html lang='en'>
       <body>
         <ConvexClientProvider>
           <Base>
             <EverwhzHeader />
-            <PlayerHolder/>
+            <PlayerHolder />
             {children}
           </Base>
         </ConvexClientProvider>
+        <div></div>
       </body>
+      {/* <div className="everwhzFooter">
+    everwhz is a visual guide to history podcasts. Every effort is made to respect all users privacy. The privacy policy is avaialble <Link className="everwhzFooterLink" href="privacy.txt">here</Link><br />
+    <Link target="_blank" href="https://icons8.com/icon/M4NWZSea5Snr/nautilus">Nautilus</Link> icon by <Link className="everwhzFooterLink" target="_blank" href="https://icons8.com">Icons8</Link>
+  </div> */}
     </html>
   );
 }
