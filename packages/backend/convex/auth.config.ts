622943b7624120369cca2dc629a427faf8b91028
Anthony Louis Burns, Esq. <burnstony@gmail.com>
2024-07-18 12:05:05 -0400

    add clerk support back to backend

--- packages/backend/convex/auth.config.ts
+++ packages/backend/convex/auth.config.ts
@@ -1,6 +1,10 @@
 export default {
   providers: [
     {
+      domain: process.env.CLERK_ISSUER_URL,
+      applicationID: "convex",
+    },
+    {
       domain: process.env.CONVEX_SITE_URL,
       applicationID: "convex",
     },
