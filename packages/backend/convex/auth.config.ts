102c3b92aad8ab4d50a31c9bc5137a520af9f6d1
Anthony Louis Burns, Esq <burnstony@gmail.com>
2025-03-07 15:41:37 -0500

    deleted CLERK_ISSUER_URL from auth.config.ts

--- packages/backend/convex/auth.config.ts
+++ packages/backend/convex/auth.config.ts
@@ -1,10 +1,6 @@
 export default {
   providers: [
     {
-      domain: process.env.CLERK_ISSUER_URL,
-      applicationID: "convex",
-    },
-    {
       domain: process.env.CONVEX_SITE_URL,
       applicationID: "convex",
     },
